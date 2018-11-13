2006a230ba843a08662956133fd3700ffaa9b3a0
Larry Boltovskoi <larry.bolt@gmail.com>
2018-11-13 15:02:43 +0100

    Add checker

---
+++ checker/team-data.rb
@@ -0,0 +1,168 @@
+team_member 'Larry' do
+  extension BasicSample
+  extension BasicScripting
+  
+  extension RayTracerV1
+  extension RayTracerV3
+  extension RayTracerV5
+
+  extension MaterialVerticalLines
+  extension MaterialGrid2d
+  
+  extension PlaneYZ
+  extension PlaneYZOptimized
+  #extension PlaneXZOptimized
+
+  extension SquareXY
+  #extension SquareXZ
+
+  extension Cube
+  
+  extension ParallelScheduler
+end
+
+team_member 'Carolyne' do
+  extension BasicSample
+  extension BasicScripting
+  
+  extension RayTracerV1
+  extension RayTracerV2
+  extension RayTracerV4
+  extension RayTracerV6
+  
+  extension RandomSampler
+  extension StratifiedSampler
+  extension JitteredSampler
+  extension HalfJitteredSampler
+  extension NRooksSampler
+  extension MultijitteredSampler
+  
+  extension MaterialCheckered2D
+  
+  extension PlaneXZ
+
+  extension SquareYZ
+  extension SquareYZOptimized
+  #extension SquareXYOptimized
+  #extension SquareXZOptimized
+
+  extension EasingLibrary
+end
+
+=begin
+team_member 'Dale' do
+  extension BasicSample
+  extension BasicScripting
+
+  extension RayTracerV6
+end
+=end
+
+
+
+# Possible extensions
+# You can copy paste lines below
+
+# extension BasicSample
+# extension BasicScripting
+# extension RayTracerV1
+# extension RayTracerV2
+# extension RayTracerV3
+# extension RayTracerV4
+# extension RayTracerV5
+# extension RayTracerV6
+# extension RandomSampler
+# extension StratifiedSampler
+# extension JitteredSampler
+# extension HalfJitteredSampler
+# extension NRooksSampler
+# extension MultijitteredSampler
+# extension DepthOfFieldCamera
+# extension FisheyeCamera
+# extension OrthographicCamera
+# extension DirectionalLight
+# extension SpotLight
+# extension AreaLight
+# extension Voronoi2D
+# extension Voronoi3D
+# extension MaterialDalmatian2D
+# extension MaterialVerticalLines
+# extension MaterialGrid2d
+# extension MaterialCheckered2D
+# extension MaterialDalmatian3D
+# extension MaterialGrid3d
+# extension MaterialCheckered3D
+# extension MaterialWorley2D
+# extension MaterialWorley3D
+# extension MaterialPerlin2D
+# extension MaterialPerlin3D
+# extension MaterialMarble2D
+# extension MaterialMarble3D
+# extension MaterialTransformer2D
+# extension MaterialTransformer3D
+# extension MaterialScale2D
+# extension MaterialScale3D
+# extension MaterialTranslate2D
+# extension MaterialTranslate3D
+# extension MaterialRotate2D
+# extension MaterialRotate3D
+# extension PlaneXZ
+# extension PlaneYZ
+# extension PlaneXZOptimized
+# extension PlaneYZOptimized
+# extension ConeAlongX
+# extension ConeAlongY
+# extension ConeAlongZ
+# extension ConeAlongXOptimized
+# extension ConeAlongYOptimized
+# extension ConeAlongZOptimized
+# extension SquareXY
+# extension SquareXZ
+# extension SquareYZ
+# extension SquareXYOptimized
+# extension SquareXZOptimized
+# extension SquareYZOptimized
+# extension Cube
+# extension CylinderAlongX
+# extension CylinderAlongY
+# extension CylinderAlongZ
+# extension CylinderAlongXOptimized
+# extension CylinderAlongYOptimized
+# extension CylinderAlongZOptimized
+# extension Triangle
+# extension TriangleOptimized
+# extension BoundingBox
+# extension Cropper
+# extension CropperOptimized
+# extension Bumpifier
+# extension Intersection
+# extension IntersectionOptimized
+# extension Difference
+# extension DifferenceOptimized
+# extension PrimitiveScaling
+# extension PrimitiveRotationX
+# extension PrimitiveRotationY
+# extension PrimitiveRotationZ
+# extension Group
+# extension Edge
+# extension Cartoon
+# extension ParallelScheduler
+# extension MotionBlur
+# extension Bmp
+# extension Ppm
+# extension EasingLibrary
+# extension Bounce
+# extension Elastic
+# extension Quadratic
+# extension Cubic
+# extension Quintic
+# extension PointAnimation
+# extension AngleAnimation
+# extension Quaternions
+# extension CircularAnimation
+# extension Lissajous
+# extension Cyclic
+# extension Slicer
+# extension MeshReading
+# extension MeshOptimizing
+# extension MeshRendering
