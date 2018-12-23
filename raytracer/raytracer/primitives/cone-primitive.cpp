3303812f38fa825341f55bfc0cda9d1e93b577f3
Larry Boltovskoi <larry.bolt@gmail.com>
2018-12-23 15:45:12 +0100

    09 Cone along y and z added and refactored code a little bit

--- raytracer/raytracer/primitives/cone-primitive.cpp
+++ raytracer/raytracer/primitives/cone-primitive.cpp
@@ -11,12 +11,38 @@ namespace
 	class ConeImplementation : public primitives::_private_::PrimitiveImplementation
 	{
 	protected:
-		ConeImplementation() {}
+		const Vector3D cone_direction_;
+		ConeImplementation(const Vector3D &direction) : cone_direction_(direction) {}
 
-		virtual void initialize_hit(Hit* hit, const Ray& ray, double t) const = 0;
-		virtual std::vector<std::shared_ptr<Hit>> find_all_hits(const Ray& ray) const = 0;
+		void initialize_hit(Hit* hit, const Ray& ray, double t) const
+		{
+			const auto factor_x = cone_direction_.x() == 1 ? -1 : 1;
+			const auto factor_y = cone_direction_.y() == 1 ? -1 : 1;
+			const auto factor_z = cone_direction_.z() == 1 ? -1 : 1;
 
+			hit->t = t;
+			hit->position = ray.origin + ray.direction * t;
+			hit->local_position.xyz = hit->position;
+
+			if (factor_x == -1)
+				hit->local_position.uv = Point2D(hit->position.y(), hit->position.z());
+			if (factor_y == -1)
+				hit->local_position.uv = Point2D(hit->position.x(), hit->position.z());
+			if (factor_z == -1)
+				hit->local_position.uv = Point2D(hit->position.x(), hit->position.y());
+
+			hit->normal = Vector3D(
+				factor_x * hit->position.x(),
+				factor_y * hit->position.y(),
+				factor_z * hit->position.z()
+			).normalized();
+		}
 	public:
+		Box bounding_box() const override
+		{
+			// TODO: not sure if this is correct
+			return Box(Interval<double>::infinite(), Interval<double>::infinite(), Interval<double>::infinite());
+		}
 		bool find_first_positive_hit(const Ray& ray, Hit* output_hit)  const override
 		{
 			auto hits = find_all_hits(ray);
@@ -35,29 +61,30 @@ namespace
 			}
 			return false;
 		}
-	};
 
-	class ConeAlongXImplementation : public ConeImplementation {
-	public:
-		ConeAlongXImplementation() = default;
-
-		Box bounding_box() const override
-		{
-			return Box(Interval<double>::infinite(), Interval<double>::infinite(), interval(-0.01, 0.01));
-		}
-
-	protected:
 		std::vector<std::shared_ptr<Hit>> find_all_hits(const Ray& ray) const override
 		{
+			// TODO: probably a nicer math way to do this
+			const auto factor_x = cone_direction_.x() == 1 ? -1 : 1;
+			const auto factor_y = cone_direction_.y() == 1 ? -1 : 1;
+			const auto factor_z = cone_direction_.z() == 1 ? -1 : 1;
+
 			std::vector<std::shared_ptr<Hit>> hits;
+			const auto a =
+				(factor_x * pow(ray.direction.x(), 2)) +
+				(factor_y * pow(ray.direction.y(), 2)) +
+				(factor_z * pow(ray.direction.z(), 2));
 
-			const auto radius = 1;
+			const auto b =
+				(factor_x * 2 * ray.origin.x()* ray.direction.x()) +
+				(factor_y * 2 * ray.origin.y() * ray.direction.y()) +
+				(factor_z * 2 * ray.origin.z() * ray.direction.z());
 
-			const auto O = Point2D(ray.origin.y(), ray.origin.z());
-			auto delta = Vector2D(ray.direction.y(), ray.direction.z());
-			const auto a = -pow(ray.direction.x(), 2) + pow(ray.direction.y(), 2) + pow(ray.direction.z(), 2);
-			const auto b = -2 * ray.origin.x()* ray.direction.x() + 2 * ray.origin.y() * ray.direction.y() + 2 * ray.origin.z() * ray.direction.z();
-			const auto c = -pow(ray.origin.x(), 2) + pow(ray.origin.y(), 2) + pow(ray.origin.z(), 2);
+			const auto c =
+				(factor_x * pow(ray.origin.x(), 2)) +
+				(factor_y * pow(ray.origin.y(), 2)) +
+				(factor_z * pow(ray.origin.z(), 2));
+
 			const auto D = pow(b, 2) - (4 * a * c);
 
 			if (D > 0)
@@ -74,118 +101,23 @@ namespace
 			}
 			return hits;
 		}
+	};
 
-		void initialize_hit(Hit* hit, const Ray& ray, double t) const override
-		{
-			const auto radius = 1;
-
-			hit->t = t;
-			hit->position = ray.origin + ray.direction * t;
-			hit->local_position.xyz = hit->position;
-			hit->local_position.uv = Point2D(hit->position.y(), hit->position.z());
-			hit->normal = Vector3D(-hit->position.x(), hit->position.y(), hit->position.z()).normalized();
-		}
+	class ConeAlongXImplementation : public ConeImplementation {
+	public:
+		ConeAlongXImplementation() : ConeImplementation(Vector3D(1, 0, 0)) { }
 	};
 
 	class ConeAlongYImplementation : public ConeImplementation {
 	public:
-		ConeAlongYImplementation() : ConeImplementation() {}
-
-		Box bounding_box() const override
-		{
-			return Box(Interval<double>::infinite(), Interval<double>::infinite(), interval(-0.01, 0.01));
-		}
-
-	protected:
-		std::vector<std::shared_ptr<Hit>> find_all_hits(const Ray& ray) const override
-		{
-			std::vector<std::shared_ptr<Hit>> hits;
-
-			const auto radius = 1;
-
-			const auto O = Point2D(ray.origin.x(), ray.origin.z());
-			auto delta = Vector2D(ray.direction.x(), ray.direction.z());
-			const auto a = delta.dot(delta);
-			const auto b = 2 * (O - Point2D(0, 0)).dot(delta);
-			const auto c = (O - Point2D(0, 0)).dot(O - Point2D(0, 0)) - (radius * radius);
-			const auto D = (b * b) - (4 * a * c);
-
-			if (D > 0)
-			{
-				auto hit1 = std::make_shared<Hit>();
-				const auto t1 = (-b - sqrt(D)) / (2 * a);
-				initialize_hit(hit1.get(), ray, t1);
-				hits.push_back(hit1);
-
-				auto hit2 = std::make_shared<Hit>();
-				const auto t2 = (-b + sqrt(D)) / (2 * a);
-				initialize_hit(hit2.get(), ray, t2);
-				hits.push_back(hit2);
-			}
-			return hits;
-		}
-
-		void initialize_hit(Hit* hit, const Ray& ray, double t) const override
-		{
-			const auto radius = 1;
-
-			hit->t = t;
-			hit->position = ray.origin + ray.direction * t;
-			hit->local_position.xyz = hit->position;
-			hit->local_position.uv = Point2D(hit->position.x(), hit->position.z());
-			hit->normal = Vector3D(0, hit->position.x() / radius, hit->position.z() / radius);
-		}
+		ConeAlongYImplementation() : ConeImplementation(Vector3D(0, 1, 0)) { }
 	};
 
 	class ConeAlongZImplementation : public ConeImplementation {
 	public:
-		ConeAlongZImplementation() : ConeImplementation() {}
-
-		Box bounding_box() const override
-		{
-			return Box(Interval<double>::infinite(), Interval<double>::infinite(), interval(-0.01, 0.01));
-		}
-
-	protected:
-		std::vector<std::shared_ptr<Hit>> find_all_hits(const Ray& ray) const override
-		{
-			std::vector<std::shared_ptr<Hit>> hits;
-
-			const auto radius = 1;
-
-			const auto O = Point2D(ray.origin.x(), ray.origin.y());
-			auto delta = Vector2D(ray.direction.x(), ray.direction.y());
-			const auto a = delta.dot(delta);
-			const auto b = 2 * (O - Point2D(0, 0)).dot(delta);
-			const auto c = (O - Point2D(0, 0)).dot(O - Point2D(0, 0)) - (radius * radius);
-			const auto D = (b * b) - (4 * a * c);
-
-			if (D > 0)
-			{
-				auto hit1 = std::make_shared<Hit>();
-				const auto t1 = (-b - sqrt(D)) / (2 * a);
-				initialize_hit(hit1.get(), ray, t1);
-				hits.push_back(hit1);
-
-				auto hit2 = std::make_shared<Hit>();
-				const auto t2 = (-b + sqrt(D)) / (2 * a);
-				initialize_hit(hit2.get(), ray, t2);
-				hits.push_back(hit2);
-			}
-			return hits;
-		}
-
-		void initialize_hit(Hit* hit, const Ray& ray, double t) const override
-		{
-			const auto radius = 1;
-
-			hit->t = t;
-			hit->position = ray.origin + ray.direction * t;
-			hit->local_position.xyz = hit->position;
-			hit->local_position.uv = Point2D(hit->position.x(), hit->position.y());
-			hit->normal = Vector3D(0, hit->position.x() / radius, hit->position.y() / radius);
-		}
+		ConeAlongZImplementation() : ConeImplementation(Vector3D(0, 0, 1)) { }
 	};
+
 }
 
 Primitive primitives::cone_along_x()
