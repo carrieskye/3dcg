4b04c31ae166b9f21f98551dcd5bbfecfced474c
Carolyne Peelman <peelmancarolyne@gmail.com>
2019-01-08 21:26:09 +0100

    09 Mesh without bb min and max

--- mesh/mesh.py
+++ mesh/mesh.py
@@ -4,15 +4,16 @@ from boundingbox import *
 
 
 def convert(file_name, max_depth, min_triangles):
-    reader = Reader("../samples/" + file_name)
+    reader = Reader("../samples/" + file_name + ".mesh")
 
     BoundingBox.index = len(reader.vertices) + len(reader.triangles) + 5
     BoundingBox.max_depth = max_depth
     BoundingBox.min_triangles = min_triangles
 
     bounding_box_hierarchy = BoundingBox.get_bb_of_triangles(reader.triangles, 1)
-    Writer("../results/" + file_name, reader, bounding_box_hierarchy)
+    Writer("../results/" + file_name + "-" + str(max_depth) + ".mesh", reader, bounding_box_hierarchy)
 
 
 # USAGE
-# convert("armadillo.mesh", 2, 3)
+# convert(<name_of_the_file>, <how_many_levels_you_want_in_the_tree>, <min_number_of_triangles_in_bottom_boxes>)
+# convert("armadillo", 20, 3)
