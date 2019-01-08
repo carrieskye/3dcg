85e798d0194e485613e3ab5775fc9452422d757e
Carolyne Peelman <peelmancarolyne@gmail.com>
2019-01-08 16:24:07 +0100

    13 Lissajous

---
+++ raytracer/raytracer/animation/lissajous.cpp
@@ -0,0 +1,20 @@
+﻿#include "animation\lissajous.h"
+
+using namespace animation;
+using namespace math;
+using namespace math;
+
+Animation<math::Point3D> animation::lissajous(animation::Duration duration, const double& xAmp, const double& yAmp, const double& zAmp, const double& xFreq, const double& yFreq, const double& zFreq, math::Angle& xPhase, math::Angle& yPhase, math::Angle& zPhase)
+{
+	auto double_animation = basic(0, 2 * atan(1) * 4, duration);
+
+	std::function<math::Point3D(TimeStamp)> lambda = [xAmp, yAmp, zAmp, xFreq, yFreq, zFreq, xPhase, yPhase, zPhase, double_animation](TimeStamp now) -> math::Point3D {
+		auto x = sin(Angle::radians(xFreq * double_animation(now)) + xPhase);
+		auto y = sin(Angle::radians(yFreq * double_animation(now)) + yPhase);
+		auto z = sin(Angle::radians(zFreq * double_animation(now)) + zPhase);
+		
+		return math::Point3D(xAmp * x, yAmp * y, zAmp * z);
+	};
+
+	return make_animation(math::from_lambda(lambda), duration);
+}
